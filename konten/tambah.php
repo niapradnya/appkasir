f0bf68a15716efabdbf53ef57d87b97c970e36e7
niapradnya <niapradnyya@gmail.com>
2024-01-17 12:48:33 +0800

    Menambahkan Aksi Keranjang & Simpan Penjualan Pada Menu Penjualan

--- konten/tambah.php
+++ konten/tambah.php
@@ -29,12 +29,14 @@
         <!-- Form Cari Produk -->
           <div class="form-row">
               <div class="form-group col-sm-2">
-              <form action="">
+              <form action="aksi/penjualan.php" method='post'>
+              <input type="hidden" name="aksi" value="tambah-keranjang-bybarcode">
+
               <input type="number" name="jumlah" class="form-control" value="1" placeholder="Jumlah...">
               </div>
 
               <div class="form-group col sm-4">
-              <input type="text" name="barcode" class="form-control" placeholder="Barcode...">
+              <input type="text" name="Barcode" class="form-control" placeholder="Barcode...">
               </div>
 
               <div class="form-group col-sm-3">
@@ -43,7 +45,7 @@
               </div>
 
               <div class="form-group col-sm-3">
-                <button class="btn btn-block btn-success" type="button"><i class="fas fa-tags"></i> Cari By Nama</button>
+                <button class="btn btn-block btn-success" type="button" data-toggle="modal" data-target="#cariProduk"><i class="fas fa-tags"></i> Cari By Nama</button>
               </div>
           </div>
         <!-- Tutup Form Cari Produk -->
@@ -59,21 +61,40 @@
               <th>Subtotal</th>
             </tr>
             </thead>
+            <?php
+            $no=0;
+            $total_item=0;
+            $total_belanja=0;
+            $id_user=$_SESSION['id'];
+            $sql_keranjang="SELECT keranjang.*,produk.NamaProduk,produk.Harga FROM keranjang,produk WHERE keranjang.ProdukID=produk.ProdukID AND id_user=$id_user";
+            $query_keranjang=mysqli_query($koneksi,$sql_keranjang);
+            while($keranjang=mysqli_fetch_array($query_keranjang)){
+              $no++;
+              $subtotal=$keranjang['Harga']*$keranjang['Jumlah'];
+              $total_item=$total_item+$keranjang['Jumlah'];
+              $total_belanja=$total_belanja+$subtotal;
+
+            ?>
             <tr>
-              <td> <a href="#"><i class="fas fa-trash"></i></a></td>
-              <td>1</td>
-              <td>Fiesta Chicken Nugget 500G</td>
-              <td align="right">34,500</td>
-              <td align="right">2</td>
-              <td align="right">69,000</td>
+              <td> <a href="aksi/penjualan.php?aksi=hapus-keranjang&ProdukID=<?=$keranjang['ProdukID']; ?>"><i class="fas fa-trash"></i></a></td>
+              <td><?= $no; ?></td>
+              <td><?= $keranjang['NamaProduk']; ?></td>
+              <td align="right"><?= number_format ($keranjang['Harga']); ?></td>
+              <td align="right"><?= number_format ($keranjang['Jumlah']); ?></td>
+              <td align="right"><?= number_format ($subtotal); ?></td>
             </tr>
+<?php
+            }
+
+?>
+
             <tr class="text-bold">
               <td colspan="4">TOTAL</td>
-              <td align="right"> 2</td>
-              <td align="right">69,000</td>
+              <td align="right"><?= number_format($total_item); ?></td>
+              <td align="right"><?= number_format($total_belanja); ?></td>
             </tr>
         </table>
-        <button class="btn btn-block bg-purple mt-3"><i class="fas fa-save"></i> Simpan Penjualan</button>
+        <button class="btn btn-block bg-purple mt-3" data-toggle="modal" data-target="#simpanJual"><i class="fas fa-save"></i> Simpan Penjualan</button>
       </div>
       </div>  
       </div><!-- /.container-fluid -->
@@ -81,88 +102,96 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-   <!-- Modal Tambah User -->
-   <div class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+
+   <!-- Modal Cari Produk -->
+   <div class="modal fade" id="cariProduk" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tambah User</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Pencarian Produk</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <form action="aksi/user.php" method="post">
-        <input type="hidden" name="aksi" value="tambah">
-    
-        <label for="nama">Nama</label>
-          <input type="text" name="nama" class="form-control" required>
-
-          <label for="username">Username</label>
-          <input type="text" name="username" class="form-control" required>
-
-          <label for="password">Password</label>
-          <input type="password" name="password" class="form-control" required>
-
-          <label for="hak_akses">Hak Akses</label>
-         <select class="form-control" name="hak_akses" id="hak_akses">
-           <option value="1">1</option>
-           <option value="2">2</option>
-         </select>
-
-          <button type="submit" class="btn btn-block bg-purple mt-3"> <i class="fas fa-save"
-          >Simpan</i></button>
-        </form>
+      <table id="example1" class="table table-hover">
+          <thead class="bg-purple">
+            <th>ID</th>
+            <th>BARCODE</th>
+            <th>NAMA PRODUK</th>
+            <th>HARGA</th>
+            <th>JUMLAH</th>
+            <th>PILIH</th>
+          </thead>
+          <?php
+            $sql="SELECT * FROM produk";
+            $query=mysqli_query($koneksi,$sql);
+            while($kolom=mysqli_fetch_array($query)){
+              ?>
+              <tr>
+                <td><?= $kolom['ProdukID']; ?></td>
+                <td><?= $kolom['Barcode']; ?></td>
+                <td><?= $kolom['NamaProduk']; ?></td>
+                <td><?= $kolom['Harga']; ?></td>
+                <td>
+                   <form action="aksi/penjualan.php" method="post">
+                      <input type="hidden" name="aksi" value="tambah-keranjang-bynama">
+                      <input type="hidden" name="ProdukID" value="<?= $kolom['ProdukID']; ?>">
+                      <input type="number" name="Jumlah" class="form-control" value="1">
+                </td>
+                <td> 
+                  <button class="btn btn-info btn-sm" type="submit"><i class="fas fa-check"></i>
+                </button>
+                </form>
+                </td>
+              </tr>
+              <?php } ?> 
+            </table>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
+        
       </div>
     </div>
   </div>
 </div>
 
-<!-- Modal Recycle Bin -->
-<div class="modal fade" id="modalRecycleBin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal Simpan Penjualan -->
+<div class="modal fade" id="simpanJual" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Data Pengapusan Sementara</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Masukkan Data Pelanggan & Waktu Transaksi</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-      <table class="table table-hover">
-          <thead class="bg-purple">
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Dihapus Pada</th>
-            <th>Aksi</th>
-          </thead>
-          <?php
-            $sql="SELECT * FROM user";
-            $query=mysqli_query($koneksi,$sql);
-            while($kolom=mysqli_fetch_array($query)){
-              ?>
-              <tr>
-                <td><?= $kolom['id_user']; ?></td>
-                <td><?= $kolom['nama']; ?></td>
-                <td><?= $kolom['dihapus_pada']; ?></td>
-                <td>  <a onclick="return confirm('Yakin akan mengembalikan data ini?')" href="aksi/user.php?aksi=restore&id_user=<?= $kolom['id_user']; ?>"> <i class="fas fa-trash-restore"></i> </a>
-                  &nbsp;
-                <a onclick="return confirm('Yakin akan menghapus data ini secara permanen?')" href="aksi/user.php?aksi=hapus-permanen&id_user=<?= $kolom['id_user']; ?>"> <i class="fas fa-eraser"></i> </a>
-              </td>
-              </tr>
-              <?php
-            } //Akhir While
-              ?>
-        </table>
+      <form action="aksi/penjualan.php" method="post">
+      <input type="hidden" name="aksi" value="simpan-penjualan">
+    <label for="PelangganID">Pelanggan</label>
+    <select name="PelangganID" class="form-control" requaired>
+            <?php 
+            $sql1="SELECT * FROM pelanggan";
+            $query1=mysqli_query($koneksi,$sql1);
+            while($pelanggan=mysqli_fetch_array($query1)){
+              echo "<option value='$pelanggan[PelangganID]'>$pelanggan[NamaPelanggan]</option>";
+            }
+            
+            ?>
+
+    </select>
+<label for="TanggalPenjualan">TanggalPenjualan</label>
+<input type="date" name="TanggalPenjualan" class="form-control" value="<?= date('Y-m-d'); ?>" required>
+<label for="TotalHarga">Total Belanja</label>      
+<input type="number" name="TotalHarga" class="form-control" value="<?= $total_belanja; ?>" readonly>
+
+<button class="btn btn-info mt-3 btn-block" type="submit"><i class="fas fa-save">Simpan Penjualan</i></button>
+</form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
        
       </div>
     </div>
   </div>
 </div>
+
